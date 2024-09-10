c8a53895c0030cf203bda04b98bad8084068fc2d
Krishna Chaitanya J <krishnachaitanyaj@Krishnas-MBP.attlocal.net>
2024-09-09 23:15:26 -0500

    prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.foodclassification import logger
 from src.foodclassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.foodclassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -10,6 +11,17 @@ if __name__=='__main__':
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    STAGE_NAME = "Prepare base model"
+    try:
+        logger.info(f"***************************")
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===============x")
     except Exception as e:
         logger.exception(e)
         raise e
---
+++ src/foodclassification/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,37 @@
+from src.foodclassification.config.configuration import ConfigurationManager
+from src.foodclassification.components.prepare_base_model import PrepareBaseModel
+from src.foodclassification import logger
+import ssl
+import urllib3
+
+STAGE_NAME = 'Prepare base model'
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        #context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        #context.check_hostname = False
+        #context.verify_mode = ssl.CERT_NONE
+    
+        # Use the context with the urllib3 PoolManager
+        #http = urllib3.PoolManager(ssl_context=context)
+
+        #vgg16 = tf.keras.applications.VGG16(weights='imagenet', include_top=False)
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"***************************")
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===============x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
